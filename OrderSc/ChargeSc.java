493e152788230c8db035198798591abb62e2642f
sunhyun <rlatjsgus08@naver.com>
2020-02-17 10:47:01 +0900

    OrderSc

---
+++ OrderSc/ChargeSc.java
@@ -0,0 +1,18 @@
+package OrderSc;
+
+import javax.swing.JFrame;
+
+public class ChargeSc extends JFrame{
+	public ChargeSc() {
+		this.setTitle("¡÷πÆ");
+		this.setBounds(30, 30, 600,300);
+		this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+		this.setVisible(true);
+	}
+	public static void main(String[] args) {
+	
+		ChargeSc CS=new ChargeSc();
+
+	}
+
+}
